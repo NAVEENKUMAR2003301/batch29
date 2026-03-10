c0d3e483a49a64bb1739daf1ff6d3147a14740d6
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-03-10 12:47:38 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,170 @@
+
+// Javascript
+
+// script
+
+// 1. jhkhkhkh
+// 2. jkbjkhb
+
+// line by code excution
+
+// single threat
+
+// sync
+
+// async
+
+
+// variable - 3 types
+
+// we can store data using container.
+
+// var     - global scope, function scope
+
+// let     - block scope 
+
+// const   - block scope
+
+
+
+//  Rules
+
+// var 
+
+
+var a = 20                      // declration   // intialiazation
+
+    a = 40                     // reuse         // reintia
+    
+var a = 60                    // redeclration   // reIntia
+
+
+console.log(a)
+
+// let    - block scope 
+
+
+let b = 30      // dec    // intia
+
+    b = 50      // reuse  // reIntia
+
+// let b        // reDec not accept let
+
+console.log(b);
+
+
+// const - constant - block scope
+
+const c = 60  
+
+    //   c = 70  // not accept reuse , reIntia
+
+// const c = 60  // not accept reDec .
+
+console.log(c);
+
+
+// example
+
+
+// let a1 = 50
+
+// const b2 = a1 
+
+//       b2 = a1 + 10   // type error
+
+
+
+// let a3 = b2
+
+// let a3 = b2
+
+// console.log(a3);  // 50
+
+
+
+// printing Statement
+
+// 1. console.log()
+
+let a4 = 100
+
+console.log(a4);
+console.log(200);
+
+
+// 2. alert() - pop stracture 
+
+
+// alert("hello team, how are you!!!")
+
+
+// 3. confirm() - pop stracture
+
+// confirm("did you like programming!!!")
+
+// 4. prompt() - get info user side
+
+// prompt("what is your name: - ")
+
+// 5. document.writeln()
+
+// var a = 50
+// document.writeln(a)
+
+
+// i want to get user name and i want show to console in thanks giving ur name.
+
+// let name1 = prompt("what is your name")
+
+// console.log("thanks giving your name :- ", name1);
+
+
+// i will ask question like , had breakfast to user side i will expect user value. 
+
+// let userBreakfast = confirm("had breakfast ?")
+
+// console.log(userBreakfast);
+
+
+// i want to know user age and i want print that age in console.
+
+// let userAge = prompt("enter your age :- ")
+
+// console.log("user age :- ", userAge);
+
+
+// console methods 
+
+
+// 1. console.log()
+
+console.log(100);
+
+
+// 2. console.warn()
+
+console.warn(100)
+
+// 3. console.error()
+
+console.error(100)
+
+
+// 4. console.clear()
+
+console.clear()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
