cff588e3658b52d75a74a0f86928e13bb5b93533
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-03-23 13:02:23 +0530

    Advance Array concept

--- style.js
+++ style.js
@@ -677,482 +677,865 @@
 //     for(let j=1; j <= 5 ; j++){
 //         console.log(j);
         
-//     }
-// }
+// //     }
+// // }
 
-// console.clear();
-
-
-// // while loop
+// // console.clear();
 
 
-// // intialization 
+// // // while loop
 
-// // while(condition){
-// //     // statement 
 
-// //     iteration
+// // // intialization 
+
+// // // while(condition){
+// // //     // statement 
+
+// // //     iteration
+// // // }
+
+
+// // // let intial = Number(prompt("enter number :- "))
+
+// // // while(intial){
+// // //     console.log(intial);
+
+// // //     intial--
+    
+// // // }
+
+
+
+// // // do while loop 
+
+// // // intialization 
+
+// // // do {
+// // //     // statement 
+// // //     iteration
+// // // }
+// // // while(condition)
+
+
+// // let in1 = 10
+
+// // do{
+// //     console.log(in1);
+// //     in1--
+    
+// // }
+// // while(in1>=30)
+
+
+
+
+
+// // // for of loop - array ,string , function
+
+// // let str = "javascript"
+// // let array = ["apple","orange","banana"]
+
+// // for(let a of str){
+
+// //     console.log(a);
+    
 // // }
 
 
-// // let intial = Number(prompt("enter number :- "))
-
-// // while(intial){
-// //     console.log(intial);
-
-// //     intial--
+// // for(let b of array){
+// //     console.log(b);
     
 // // }
 
 
 
-// // do while loop 
+// // // for in loop - object
 
-// // intialization 
-
-// // do {
-// //     // statement 
-// //     iteration
-// // }
-// // while(condition)
-
-
-// let in1 = 10
-
-// do{
-//     console.log(in1);
-//     in1--
-    
-// }
-// while(in1>=30)
-
-
-
-
-
-// // for of loop - array ,string , function
-
-// let str = "javascript"
-// let array = ["apple","orange","banana"]
-
-// for(let a of str){
-
-//     console.log(a);
-    
-// }
-
-
-// for(let b of array){
-//     console.log(b);
-    
-// }
-
-
-
-// // for in loop - object
-
-// let object = {
-//     name1 : "ben",
-//     role : "developer",
-//     salary : 100000
-// }
-
-// for(let a in object){
-//     console.log(object[a]);
-    
-// }
-
-// console.clear();
-
-
-// // 3. function 
-
-
-// // block of code
-
-// // reusable
-
-// // Easy to readable , clean
-
-// // Repeation code avoid 
-
-// // when you call that time only render
-
-
-// // syntex 
-
-
-// // function one(parameter){
-// //     // statement
-// //     // return
-// //     // yeild
+// // let object = {
+// //     name1 : "ben",
+// //     role : "developer",
+// //     salary : 100000
 // // }
 
+// // for(let a in object){
+// //     console.log(object[a]);
+    
+// // }
 
-// // one(arguments)
+// // console.clear();
 
 
-// function one(){
-//     console.log("hello");
+// // // 3. function 
 
-//     console.log("hi");
 
-//     for(let i = 0 ; i<=6 ; i++){
-//         console.log(i)
-//     }
+// // // block of code
 
-//     if(1){
-//         console.log("true");
+// // // reusable
+
+// // // Easy to readable , clean
+
+// // // Repeation code avoid 
+
+// // // when you call that time only render
+
+
+// // // syntex 
+
+
+// // // function one(parameter){
+// // //     // statement
+// // //     // return
+// // //     // yeild
+// // // }
+
+
+// // // one(arguments)
+
+
+// // function one(){
+// //     console.log("hello");
+
+// //     console.log("hi");
+
+// //     for(let i = 0 ; i<=6 ; i++){
+// //         console.log(i)
+// //     }
+
+// //     if(1){
+// //         console.log("true");
         
+// //     }
+    
+    
+// // }
+
+// // one()
+
+
+
+
+// // function two(a){
+// //     console.log(a);
+    
+// // }
+
+// // two("welcome")
+
+
+
+// // function form(name,department,number,stay){
+
+// //     console.log("name :-",name );
+// //     console.log("department :-",department );
+// //     console.log("Number :-",number );
+// //     console.log("stay :-",stay );
+    
+// // }
+
+// // form("kamal","ece",8878987663,"dayscholar")
+// // form("praveen","cse",75765756465,"hostel")
+
+
+// // function additional(name){
+
+// //     return name
+
+// // }
+
+// // let returnVal = additional("ben")
+
+// // console.log(returnVal);
+
+
+
+// // function extra(a){
+
+// //     console.log("extra :- ",returnVal);
+
+// //     console.log(a);
+    
+    
+// // }
+
+// // extra(returnVal);
+
+
+
+// // let obj = {
+// //     name : "john",
+// //     role : "developer"
+// // }
+
+
+// // function callOut(){
+
+// //     console.log(obj.name);
+// //     console.log(obj.role);
+    
+    
+// // }
+
+// // callOut()
+
+// // console.clear();
+
+
+
+
+// // // scope :- 
+
+// // function scopeCheck(){
+// //     if(true){
+// //         var z1 = "global scope"
+// //         let z2 = "block scope"
+// //         const z3 = "block scope const"
+// //         console.log(z2);
+// //         console.log(z3);
+// //     }
+// //     console.log(z1);
+    
+
+    
+// // }
+
+// // scopeCheck()
+
+
+// // // hoisting
+
+// // console.log(y1);
+// // var y1 = "global scope var"
+
+
+
+// // let y2 = "block scope let"
+// // console.log(y2);
+
+
+
+// // const y3 = "block scope const"
+// // console.log(y3);
+
+// // // function Types :- 
+
+
+// // // 1. named function
+
+// // named("named function")
+// // function named(parameter){
+
+// //     console.log(parameter);
+    
+// // }
+
+
+// // // 2. annonymous function
+
+// // var annonymous = function(parameter){
+    
+// //     console.log(parameter);
+    
+// // }
+// // annonymous("annoymous function")
+
+
+// // // 3. arrow function
+
+// // var arrow = (parameter)=>{
+    
+// //     console.log(parameter)
+// // }
+
+// // arrow("arrow function")
+
+
+// // // how to set default parameter
+
+// // function form1(name,department,disability = "no"){
+
+
+// //     console.log("name :- ", name);
+// //     console.log("department :- ",department);
+// //     console.log("isDisability :- ",disability);
+    
+    
+    
+// // }
+
+// // form1("john","ece","yes")
+// // form1("ben","cse")
+
+
+// // // IIFE - self invoke function
+
+// // (function(parameter){
+
+// //     statement 
+// //     yield 
+// //     return
+
+// // })("argument")
+
+
+// // (function(product,discount){
+// //     alert(`now black day sale ongoing , ${product} now this product ongoing ${discount} % discounts have now....`)
+// // })("tShirt",70)
+
+
+
+// // Higherorder Function and Callback function
+
+// function function1(){
+//     console.log("higherorder function");
+    
+// }
+
+// function function2(){
+//     console.log("callback function");
+    
+// }
+
+// function1(function2());
+
+
+
+
+
+
+
+// // example
+
+// function add(callback,a,b){ // callback = sub , a = 20 , b = 40
+//     console.log(a+b); // 20 + 40 = 60
+
+//     callback(20,20)  // sub(20,20)
+    
+    
+// }
+
+// function sub(num1,num2){ // n1 = 20 , n2 = 20
+//     console.log(num1-num2);// 20-20 = 0
+    
+// }
+
+
+// add(sub,20,40) 
+
+
+
+// // currying format 
+
+
+// function d1(a){
+//     return function (b){
+//         return function (c){
+//             console.log(a + b + c);
+            
+//         }
 //     }
-    
-    
 // }
 
-// one()
+// d1(1)(2)(3)
 
 
+// // generator function 
 
 
-// function two(a){
+// function* cashback(){
+//     yield "you will get 20rs"
+//     yield "you will get 6rs"
+//     yield "try again better luck next time"
+//     yield "congradulation you will get 100rs"
+// }
+
+// let collectMoney = cashback()
+
+// // console.log(collectMoney.next().value);
+// // console.log(collectMoney.next().value);
+// // console.log(collectMoney.next().value);
+// // console.log(collectMoney.next().value);
+
+// // for of 
+
+// for(let a of collectMoney){
+
 //     console.log(a);
     
 // }
 
-// two("welcome")
 
 
-
-// function form(name,department,number,stay){
-
-//     console.log("name :-",name );
-//     console.log("department :-",department );
-//     console.log("Number :-",number );
-//     console.log("stay :-",stay );
-    
-// }
-
-// form("kamal","ece",8878987663,"dayscholar")
-// form("praveen","cse",75765756465,"hostel")
+// // switch 
 
 
-// function additional(name){
+// // // switch(condition){
+// // //     case value : statement :break
+// // //     case value : statement :break
+// // //     case value : statement :break
+// // //     case value : statement :break
+// // // }
 
-//     return name
+// // let day = Number(prompt("enter day in number"))
 
-// }
+// // switch(day){
+// //     case 1 : alert("sunday") ;  break;
+// //     case 2 : alert("monday") ;  break;
+// //     case 3 : alert("tueday") ;  break;
+// //     case 4 : alert("wedday") ;  break;
+// //     case 5 : alert("thursday") ;  break;
+// //     case 6 : alert("friday") ;  break;
+// //     case 7 : alert("saturday") ;  break;
 
-// let returnVal = additional("ben")
+// //     default : alert("good day")
+// // }
 
-// console.log(returnVal);
-
-
-
-// function extra(a){
-
-//     console.log("extra :- ",returnVal);
-
-//     console.log(a);
-    
-    
-// }
-
-// extra(returnVal);
-
-
-
-// let obj = {
-//     name : "john",
-//     role : "developer"
-// }
-
-
-// function callOut(){
-
-//     console.log(obj.name);
-//     console.log(obj.role);
-    
-    
-// }
-
-// callOut()
 
 // console.clear();
 
 
+// // DataStracture (ES6) --> Ecma script 6th edition -- 2015
 
 
-// // scope :- 
+// // spread operator 
 
-// function scopeCheck(){
-//     if(true){
-//         var z1 = "global scope"
-//         let z2 = "block scope"
-//         const z3 = "block scope const"
-//         console.log(z2);
-//         console.log(z3);
-//     }
-//     console.log(z1);
-    
 
-    
+// // array , object 
+
+
+// // we can take clone and merge the value
+
+
+// // array spread operator - [...]
+
+
+// let array1 = [1,2,3,4]
+// let array2 = [5,6,7,8]
+
+// let newArray = [...array1,...array2,9,10]
+
+// console.log(newArray);
+
+
+// // object spread operator - {...}
+
+
+// let obj1 = {
+//     name1 : "ismail",
+//     role  : "trainee",
+//     salary: 30000
 // }
 
-// scopeCheck()
-
-
-// // hoisting
-
-// console.log(y1);
-// var y1 = "global scope var"
-
-
-
-// let y2 = "block scope let"
-// console.log(y2);
-
-
-
-// const y3 = "block scope const"
-// console.log(y3);
-
-// // function Types :- 
-
-
-// // 1. named function
-
-// named("named function")
-// function named(parameter){
-
-//     console.log(parameter);
-    
+// let obj2 = {
+//     name11 : "ismail",
+//     role2 : "developer",
+//     salary3 : 50000
 // }
 
 
-// // 2. annonymous function
+// let storeInfoInLog = {...obj1,...obj2}
 
-// var annonymous = function(parameter){
-    
-//     console.log(parameter);
-    
-// }
-// annonymous("annoymous function")
+// console.log(storeInfoInLog);
 
 
-// // 3. arrow function
+// // Rest operator - (...)
 
-// var arrow = (parameter)=>{
-    
-//     console.log(parameter)
-// }
+// // function 
 
-// arrow("arrow function")
+// // we can store multiple argument in single parameter
 
+// function AvoidEmptyStorage(a,b,...c){
 
-// // how to set default parameter
-
-// function form1(name,department,disability = "no"){
-
-
-//     console.log("name :- ", name);
-//     console.log("department :- ",department);
-//     console.log("isDisability :- ",disability);
+//     console.log(a+b+c[0]+c[1]);
+//     console.log(c);
     
     
-    
+
 // }
 
-// form1("john","ece","yes")
-// form1("ben","cse")
+// AvoidEmptyStorage(1,2,3,4,5,6,7,8)
+
+// console.clear();
 
 
-// // IIFE - self invoke function
-
-// (function(parameter){
-
-//     statement 
-//     yield 
-//     return
-
-// })("argument")
+// Destracture
 
 
-// (function(product,discount){
-//     alert(`now black day sale ongoing , ${product} now this product ongoing ${discount} % discounts have now....`)
-// })("tShirt",70)
+// array 
+
+let arr1 = [1,2,3,4]
+
+let a1 = arr1[0]
+let a2 = arr1[1]
+let a3 = arr1[2]
+let a4 = arr1[3]
+
+console.log(a4 + a1);
 
 
+// deStracture Array 
 
-// Higherorder Function and Callback function
+let [b1,b2,b3,b4] = arr1
 
-function function1(){
-    console.log("higherorder function");
-    
-}
-
-function function2(){
-    console.log("callback function");
-    
-}
-
-function1(function2());
+console.log(b1 + b4);
 
 
+// nested Array 
 
 
+let nested = [1,2,3,[4,5,[6,7,[8]]]]
 
+let c1 = nested[0]
+let c2 = nested[1]
+let c3 = nested[2]
+let c4 = nested[3][0]
+let c5 = nested[3][1]
+let c6 = nested[3][2][0]
+let c7 = nested[3][2][1]
+let c8 = nested[3][2][2][0]
 
-
-// example
-
-function add(callback,a,b){ // callback = sub , a = 20 , b = 40
-    console.log(a+b); // 20 + 40 = 60
-
-    callback(20,20)  // sub(20,20)
-    
-    
-}
-
-function sub(num1,num2){ // n1 = 20 , n2 = 20
-    console.log(num1-num2);// 20-20 = 0
-    
-}
-
-
-add(sub,20,40) 
+console.log(c2);
 
 
 
-// currying format 
+let [d1,d2,d3,[d4,d5,[d6,d7,[d8]]]] = nested
+
+console.log(d2);
 
 
-function d1(a){
-    return function (b){
-        return function (c){
-            console.log(a + b + c);
-            
-        }
-    }
-}
+// object 
 
-d1(1)(2)(3)
-
-
-// generator function 
-
-
-function* cashback(){
-    yield "you will get 20rs"
-    yield "you will get 6rs"
-    yield "try again better luck next time"
-    yield "congradulation you will get 100rs"
-}
-
-let collectMoney = cashback()
-
-// console.log(collectMoney.next().value);
-// console.log(collectMoney.next().value);
-// console.log(collectMoney.next().value);
-// console.log(collectMoney.next().value);
-
-// for of 
-
-for(let a of collectMoney){
-
-    console.log(a);
-    
+let obj = {
+    title : "batch 29",
+    count : 35,
+    description : "march 2"
 }
 
 
+let obj1 = obj.title
+let obj2 = obj.count
+let obj3 = obj.description
 
-// switch 
+console.log(obj1,obj2,obj3);
 
 
-// // switch(condition){
-// //     case value : statement :break
-// //     case value : statement :break
-// //     case value : statement :break
-// //     case value : statement :break
-// // }
+// obj destracture 
 
-// let day = Number(prompt("enter day in number"))
 
-// switch(day){
-//     case 1 : alert("sunday") ;  break;
-//     case 2 : alert("monday") ;  break;
-//     case 3 : alert("tueday") ;  break;
-//     case 4 : alert("wedday") ;  break;
-//     case 5 : alert("thursday") ;  break;
-//     case 6 : alert("friday") ;  break;
-//     case 7 : alert("saturday") ;  break;
+let { description, title, count } = obj;
 
-//     default : alert("good day")
-// }
+console.log(description, title, count);
+
+console.clear();
+
+
+// Array advance concept
+
+let normalArray = [1,2,3,4,"hello",true,undefined,null,[1,2],{k:1}]
+
+console.log(normalArray);
+console.log(normalArray[0]);
+console.log(normalArray[normalArray.length-1]);
+
+
+// homogeneous
+
+// hetrogeneous
+
+// flexible
+
+// array manipulation method :-
+
+// add method - array inside if you add you can add multiple value
+
+// remove method - array inside if you use remove method you can remove only one value.
+
+
+let example = [1,2,3]
+
+// push()    - array last value we can add
+
+example.push(4,5,6,7,8)
+
+// pop()     - array last value we can remove
+
+example.pop()
+
+// shift()   - array first value we can remove
+
+example.shift()
+
+// unshift() - array first value we can add
+
+example.unshift(0,1)
+
+
+console.log(example);
+
+
+// splice(startingIndex,remove count,adding Value)
+
+let newArr = [1,2,3,44,55,66,7,8,9]
+
+newArr.splice(3,3)
+
+
+console.log(newArr);
+
+
+
+// Array merge method 
+
+
+// concat()
+
+let aa1 = [1,2,3,4]
+let aa2 = [5,6,7,8] 
+
+
+let newArr1 = aa1.concat(aa2)
+
+console.log(newArr1);
+
+
+
+//  slice() - startingIndex , EndingIndex + 1
+
+
+let arr2 = [1,2,3,4000,5000,6000,7,8,9,10]
+
+let sliceVal = arr2.slice(3,6)
+
+console.log(sliceVal);
+
+
+
+//  flat()
+
+
+let nest = [1, 2, 3, [4, 5, [6]]];
+
+let flatVal = nest.flat(Infinity);
+
+console.log(flatVal);
+
+
+
+
+
+//  fill()
+
+let arr3 = [1,2,3,4] // 1 , "two" , 3 , 4
+
+arr3.fill("two",1,2) // value , si , ei + 1
+
+console.log(arr3);
+
+
+
+
+
+//  includes() 
+
+let arr4 = [1,2,40,60,11,4]
+
+let includesVal = arr4.includes(20)
+
+console.log(includesVal);
+
+
+// sort()
+
+let arr5 = [3,5,2,8,0,1,4]
+
+let sortval = arr5.sort()
+
+//  reverse()
+
+console.log(sortval.reverse());
+
+let arr6 = [2,44,55,6000,1,5,9]
+
+let sort1 = arr6.sort()
+
+console.log(sort1); // 4.4 < 5 --> true
+
+
+//  indexOf()
+
+let arr7 = [1,2,3,4,3,2,1]
+
+let ind = arr7.indexOf(3,3) // value , fromIndex
+
+console.log(ind);
+
+
+
+//  lastIndexOf()
+
+let arr8 = [1,2,3,4,5,4,3,2,1]
+
+let last = arr8.lastIndexOf(3,5) // value , fromindex
+
+console.log(last);
+
+
 
 
 console.clear();
 
 
-// DataStracture (ES6) --> Ecma script 6th edition -- 2015
+// Array higherorder methods
+
+let games = ["kabadi","cricket","chess","carrom"]
+
+// forEach()
+
+let newForEach = games.forEach((currentElement,index,TotalArray)=>{
+    return currentElement
+   
+})
+
+console.log(newForEach);
 
 
-// spread operator 
+// map()
 
-
-// array , object 
-
-
-// we can take clone and merge the value
-
-
-// array spread operator - [...]
-
-
-let array1 = [1,2,3,4]
-let array2 = [5,6,7,8]
-
-let newArray = [...array1,...array2,9,10]
-
-console.log(newArray);
-
-
-// object spread operator - {...}
-
-
-let obj1 = {
-    name1 : "ismail",
-    role  : "trainee",
-    salary: 30000
-}
-
-let obj2 = {
-    name11 : "ismail",
-    role2 : "developer",
-    salary3 : 50000
-}
-
-
-let storeInfoInLog = {...obj1,...obj2}
-
-console.log(storeInfoInLog);
-
-
-// Rest operator - (...)
-
-// function 
-
-// we can store multiple argument in single parameter
-
-function AvoidEmptyStorage(a,b,...c){
-
-    console.log(a+b+c[0]+c[1]);
-    console.log(c);
+let newMap = games.map((c,i,t)=>{
+    return c
     
-    
+})
 
-}
+console.log(newMap);
 
-AvoidEmptyStorage(1,2,3,4,5,6,7,8)
+
+
+
+// filter()
+
+let employee = [
+    {name1 : "a",salary : 100000},
+    {name1 : "b",salary : 200000},
+    {name1 : "c",salary : 300000},
+    {name1 : "d",salary : 400000},
+    {name1 : "e",salary : 500000},
+    {name1 : "f",salary : 600000},
+    {name1 : "g",salary : 700000}
+]
+
+let salaryDate = employee.filter((c,i,t)=>{
+  return c.salary >= 500000
+})
+
+console.log(salaryDate);
+
+
+// find()
+
+let firstGotSalary = employee.find((c,i,t)=>{
+return c.salary < 500000
+})
+
+console.log(firstGotSalary);
+
+
+// reduce
+
+employee = [
+  { name1: "a", salary: 100000 },
+  { name1: "b", salary: 200000 },
+  { name1: "c", salary: 300000 },
+  { name1: "d", salary: 400000 },
+  { name1: "e", salary: 500000 },
+  { name1: "f", salary: 600000 },
+  { name1: "g", salary: 700000 },
+];
+
+let allSalary = employee.reduce((acc,c,i,t)=>{
+
+    return acc + c.salary
+
+    //     0  + 1  = 1
+    //     1  + 2  = 3
+    //     3  + 3  = 6
+    //     6  + 4  = 10
+    //     10 + 5  = 15
+    //     15 + 6  = 21
+    //     21 + 7  = 28
+    // returb 28
+
+  
+
+},0) // 1 // 3 // 6 // 10 // 15 // 21 // 28
+
+console.log(allSalary);
+
+
+
+// some() - or 
+
+let arr9 = [10,3,4,5,7]
+
+let someVal = arr9.some((c,i,t)=>{
+
+    return c % 2 == 0 
+
+    // 10 % 2 == 0 true 
+    // 3 % 2  == 0 false
+    // 4 % 2 == 0 true
+
+    // true || false || true || false || false = true
+
+})
+
+console.log(someVal);
+
+
+
+// every() - and
+
+let arr10 = [2,4,6,8]
+
+let everyVal = arr10.every((c,i,t)=>{
+
+    return c % 2 == 0 
+
+})
+
+console.log(everyVal);
+
+
+// sort()
+
+let arr11 = [232,545,4,6,45,2,56]
+
+let sort11 = arr11.sort((a,b)=>{
+  return b-a
+})
+
+
+console.log(sort11);
+
+
+// convert arr to str
+
+//  toString()
+
+let arr12 = [1,2,3,4]
+
+console.log(arr12.toString());
+
+// join()
+
+console.log(arr12.join("hello"));
+
+
+
+
+
+
+
+
+
 
 
 
